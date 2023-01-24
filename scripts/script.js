4d023a1799e54611d4a5d3a246bef67992a3a851
filipearray <codedforgetbro@gmail.com>
2023-01-23 22:06:45 -0300

    fields validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,54 @@
-const registerNewClient = document.getElementById("registerNewClient");
-const modalClose = document.getElementById("modalClose");
+const tempClient = {
+    nome: "Filipe",
+    email: "filipeguimaraes@gmail.com",
+    celular: "990297113",
+    cidade: "Santa City"
+};
 
-const openModal = () => document.getElementById("modal").classList.add("active");
-const closeModal = () => document.getElementById("modal").classList.remove("active");
+const getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
 
-registerNewClient.addEventListener("click", openModal);
-modalClose.addEventListener("click", closeModal);
+// CREATE
+const createClient = (client) => {
+    const dbClient = getLocalStorage();
+    dbClient.push(client);
+    setLocalStorage(dbClient);
+};
+
+// READ
+const readClient = () => getLocalStorage(); 
+
+// UPDATE
+const updateClient = (index, client) => {
+    const dbClientUpdated = readClient();
+    dbClientUpdated[index] = client;
+    setLocalStorage(dbClientUpdated);
+};
+
+// DELETE
+const deleteClient = (index) => {
+    const dbClientDeleted = readClient();
+    dbClientDeleted.splice(index, 1);
+    setLocalStorage(dbClientDeleted);
+};
+
+// Layout interaction
+
+// Step 1 - Vamos começar criando a feature em que nós clicamos em salvar e as informações são salvas na database. Precisamos primeiro nos certificar de que todos os campos serão salvos (Isso será verificado na func isValidFields).
+
+// o reportValidity retorna true ou false. Verifica se as regras são seguidas, se todos os requisitos foram atendidos.
+const saveButton = document.getElementById("saveBtn")
+
+saveButton.addEventListener("click", saveClient)
+
+const isValidFields = () => {
+    const form = document.getElementById("idForm");
+
+    return form.reportValidity();
+}
+
+const saveClient = () => {
+    if(isValidFields()){
+        console.log("x");
+    }
+};
